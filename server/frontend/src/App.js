62835eeb51c82b478306d41d24778f6f50863dfe
junaidmemon1 <junaidmushtaquememon@gmail.com>
2025-12-08 03:34:06 -0500

    Added registration view and frontend routes

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,12 @@
 import LoginPanel from "./components/Login/Login"
+import RegisterPanel from "./components/Register/Register"   // <-- Add this import
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<RegisterPanel />} />   {/* <-- New route */}
     </Routes>
   );
 }
