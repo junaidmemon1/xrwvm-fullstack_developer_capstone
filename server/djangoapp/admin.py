de6d8ecc5a455fc523360072711cf3f48dd210a1
junaidmemon1 <junaidmushtaquememon@gmail.com>
2025-12-10 03:03:59 -0500

    Add CarModel and CarMake Django models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,27 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
-# Register your models here.
+# Inline model to display CarModels inside CarMake admin page
+class CarModelInline(admin.StackedInline):  # You can use admin.TabularInline if preferred
+    model = CarModel
+    extra = 1  # Number of empty forms displayed
 
-# CarModelInline class
 
-# CarModelAdmin class
+# Custom admin view for CarModel
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year', 'dealer_id')
+    list_filter = ('car_make', 'type', 'year')
+    search_fields = ('name', 'car_make__name')
 
-# CarMakeAdmin class with CarModelInline
+
+# Custom admin view for CarMake with inline CarModels
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    inlines = [CarModelInline]
+
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,42 +1,67 @@
 # Uncomment the required imports before adding the code
 
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
-# from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
-# from django.contrib.auth import logout
-# from django.contrib import messages
-# from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth import logout
+from django.contrib import messages
+from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
 
+# Import your models
+from .models import CarMake, CarModel
 
-# Get an instance of a logger
+# Import initiate() if you have populate.py
+from .populate import initiate
+
+# Logger instance
 logger = logging.getLogger(__name__)
 
 
-# Create your views here.
-
-# Create a `login_request` view to handle sign in request
+# ------------------------------ LOGIN VIEW ------------------------------
 @csrf_exempt
 def login_user(request):
-    # Get username and password from request.POST dictionary
+    # Parse JSON credentials
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
-    # Try to check if provide credential can be authenticated
+
     user = authenticate(username=username, password=password)
-    data = {"userName": username}
+    response = {"userName": username}
+
     if user is not None:
-        # If user is valid, call login method to login current user
         login(request, user)
-        data = {"userName": username, "status": "Authenticated"}
-    return JsonResponse(data)
+        response["status"] = "Authenticated"
+
+    return JsonResponse(response)
+
+
+# ------------------------------ GET CARS VIEW ------------------------------
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+
+    # Auto-populate cars if DB empty
+    if count == 0:
+        initiate()
+
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+
+    for car_model in car_models:
+        cars.append({
+            "CarModel": car_model.name,
+            "CarMake": car_model.car_make.name
+        })
+
+    return JsonResponse({"CarModels": cars})
+
 
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
